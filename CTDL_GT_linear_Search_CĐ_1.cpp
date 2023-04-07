fd934230e892618a2d4127d1f1f1baf56e5f75c8
Votankiet <125332988+Votankiet@users.noreply.github.com>
2023-04-07 08:14:13 +0700

    Add files via upload

---
+++ CTDL_GT_linear_Search_CĐ_1.cpp
@@ -0,0 +1,14 @@
+//Cài đặt tìm kiếm tuyến tính
+//link tham khao: https://gochocit.com/ky-thuat-lap-trinh/thuat-toan-tim-kiem-tuyen-tinh-linear-search
+int LinearSearch(int a[], int n, int x){
+	int i=0; a[n]=x; //a[n] l� ph?n t? �l�nh canh�
+	while(a[i]!=x){
+		i++;
+	}
+	if(i==n){
+		return -1; //T�m kh�ng th?y x
+	}
+	else{
+		return i; //T�m th?y x
+	}
+}
