bbdf5da95f526521e4e32ae2c470ed84ddc7541c
Votankiet <125332988+Votankiet@users.noreply.github.com>
2023-04-07 08:24:23 +0700

    Add files via upload

---
+++ CTDL_GT_Interchange_Sort_1.cpp
@@ -0,0 +1,12 @@
+//Cài đặt của Interchange Sort
+//link tham khao: https://gochocit.com/ky-thuat-lap-trinh/thuat-toan-sap-xep-doi-cho-truc-tiep-interchange-sort
+void Interchange_Sort(int a[], int n){
+	int i,j;
+	for(i=0; i<n-1; i++){
+		for(j=i+1; j<n; j++){
+			if(a[i]>a[j]){
+				swap(a[i],a[j]);//hoán vi a[i] và a[j]
+			}
+		}
+	}
+}
